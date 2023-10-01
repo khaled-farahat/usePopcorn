ab3c98740e16fbbee3972184fb1c983819125fda
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2023-10-01 07:31:10 +0300

    fix deployment

--- src/utils/average.ts
+++ src/utils/average.ts
@@ -1,4 +1,4 @@
 const average = (arr: number[]) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length + (i * 0), 0);
 
 export default average;
